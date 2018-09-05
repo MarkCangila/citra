8534399db16415ec48ae81f3555b5ddcd17729d8
MarkCangila <38412104+MarkCangila@users.noreply.github.com>
2018-09-05 18:08:31 -0400

    Add function to header

--- src/input_common/sdl/sdl.h
+++ src/input_common/sdl/sdl.h
@@ -35,6 +35,8 @@ void HandleGameControllerEvent(const SDL_Event& event);
 /// A Loop that calls HandleGameControllerEvent until Shutdown is called
 void PollLoop();
 
+std::vector<std::string> GetDefaultMapping(); 
+  
 /// Creates a ParamPackage from an SDL_Event that can directly be used to create a ButtonDevice
 Common::ParamPackage SDLEventToButtonParamPackage(const SDL_Event& event);
 
